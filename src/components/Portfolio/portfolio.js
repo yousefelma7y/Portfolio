82f18f31c3207fdc515cd8ad654a0fa84d282c56
yousefelma7y <yousefelmahy7112000@gmail.com>
2022-10-01 20:57:24 +0200

    add new project

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -6,38 +6,48 @@ import './portfolio.css';
 import {motion} from 'framer-motion/dist/es/index' ;
 
 const portfolio = () => {
+
+  const projects = [
+    {
+      id: 1 ,
+      class: 'projects first-proj col-md-3 col-sm-5 col-9' ,
+      link : 'https://ecommerce-souq.netlify.app/'
+    } ,
+    {
+      id: 2 ,
+      class: 'projects secound-proj col-md-3 col-sm-5 col-9'  ,
+      link : 'https://damanhour-football.netlify.app/'
+    },
+    {
+      id: 3 ,
+      class: 'projects third-proj col-md-3 col-sm-5 col-9'  ,
+      link : 'https://travelagency711.000webhostapp.com/'
+    },
+ 
+  ]
+    
+  
+
   return (
     <div className="portfolio-page justify-content-center text-center  w-100" id='portfolio'>
       <motion.h1 initial={{opacity:0 ,y:-50 ,x:-60}} animate={{opacity:1 ,y:0 ,x:0 }} transition={{delay:0.05}} className='fw-bold pt-5'>
         My Portfolio
       </motion.h1>
       <Row className='justify-content-center mt-5 w-100'>
-      <motion.div initial={{opacity:0 ,y:-50 ,x:-50}} animate={{opacity:1 ,y:0 ,x:0 }} transition={{delay:0.05}}   whileHover={{y:-10}} 
-       className='projects first-proj col-md-3 col-sm-5 col-9 '>
-         <p className="overlay">
-              <button className="show-project" onClick={()=> window.open("https://damanhour-football.netlify.app/", "_blank")}>
-                  Show Project
-              </button>
-          </p>
-        </motion.div>
-        <motion.div initial={{opacity:0 ,y:-50 ,x:-50}} animate={{opacity:1 ,y:0 ,x:0 }} transition={{delay:0.1}}   whileHover={{y:-10}}
-         className='projects secound-proj col-md-3 col-sm-5 col-9'>
-         <p className="overlay ">
-          <button className="show-project" onClick={()=> window.open("https://travelagency711.000webhostapp.com/", "_blank")} >
-                    Show Project
-           </button>
-          </p>
-          </motion.div>
-          <motion.div initial={{opacity:0 ,y:-50 ,x:-50}} animate={{opacity:1 ,y:0 ,x:0 }} transition={{delay:0.15}}  whileHover={{y:-10}}
-           className='projects third-proj col-md-3 col-sm-5 col-9'>
-          <p className="overlay ">
-          <button className="show-project " >
-            <Link className="showimage" to="/school">Show Image</Link>
-           </button>
-          </p>
-          </motion.div>
-        
-      
+
+      {
+        projects.map(projects =>  
+        <motion.div initial={{opacity:0 ,y:-50 ,x:-50}} animate={{opacity:1 ,y:0 ,x:0 }} transition={{delay:0.05}}   whileHover={{y:-10}} 
+
+         key={projects.id} className={projects.class} >
+            <p className="overlay">
+                 <button className="show-project" onClick={()=> window.open( projects.link , "_blank")}>
+                     Show Project
+                 </button>
+             </p>
+           </motion.div>)
+      }
+
       </Row>
      
     </div>
